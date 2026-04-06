e1a9e4d1eac434a33315b22d229cb9c71f755e99
sonal <sonalbagdiya@gmail.com>
2026-04-06 09:11:20 +0530

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,15 @@
+terraform {
+  required_providers {
+    local = {
+      source = "hashocorp/local"
+      version = "~> 2.5"
+      }
+  }
+}
+resource "local_file" "example" {
+  filename = "$path.module/hello_world.txt"
+  content = "This file was created by Terrafrom"
+}
+output "file_path" {
+value = local_file.example.filename
+}
